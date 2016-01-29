c9c2303fc5a3e3806afb996d17b905a2a619603d
rangermann <rainer@rarebyte.com>
2016-01-29 20:50:13 +0100

    added state machine and game controller

---
+++ src/Assets/scripts/game/GameController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+  private static GameController instance;
+  public static GameController Instance {
+    get {
+      return instance;
+    }
+  }
+
+  private StateMachine StateMachine { get; set; }
+
+  public void Awake() {
+    instance = this;
+    DontDestroyOnLoad(transform.parent.gameObject);
+
+    List<AbstractState> states = new List<AbstractState>();
+    states.Add(new GameStateLoading("GameStateLoading"));
+    // add more game states here
+
+    StateMachine = StateMachine.Create("state_machine", states, "GameStateLoading");
+  }
+
+  public void ChangeState(string stateName, object onEnterParams = null) {
+    StateMachine.ChangeState(stateName, onEnterParams);
+  }
+
+  public void OnGUI() {
+#if UNITY_EDITOR
+    GUILayout.Label("Current state: " + StateMachine.CurrentStateName);
+#endif
+  }
+
+}
+
---
+++ src/Assets/scripts/game/states/GameStateLoading.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+public class GameStateLoading : AbstractState {
+
+  public GameStateLoading(string stateName)
+    : base(stateName) {
+  }
+
+  protected override void OnInitialize() {
+
+  }
+
+  protected override void OnEnter(object onEnterParams = null) {
+
+  }
+
+  protected override void OnLeave() {
+
+  }
+
+  protected override void OnUpdate() {
+
+  }
+}
+
