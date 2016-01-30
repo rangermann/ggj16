1fe711bb46a975045aded7920ff361ca1aabff6b
Johanna Pirker <joeyprink@gmail.com>
2016-01-30 16:31:13 +0100

    second control added

--- src/Assets/scripts/game/GameConfig.cs
+++ src/Assets/scripts/game/GameConfig.cs
@@ -6,14 +6,15 @@ public class GameConfig : MonoBehaviour {
   public float cameraMovementSpeed = 5.0f;
 
   [Header("Player")]
-  public float playerScaleDownFactor = 0.5f;
-  public float playerScaleUpFactor = 0.7f;
+  public float playerScaleDownFactor = 0.5f; //0.5f for pressing control; else 8 
+  public float playerScaleUpFactor = 0.7f; //0.7f for pressing control; else 0..3f 
   public float playerMinScale = 0.5f;
   public float playerMaxScale = 5.0f;
   public float playerMinVelocityDelta = -3f;
   public float playerMaxVelocityDelta = 0.4f;
   public float playerMaxCameraOffset = 4f;
   public float playerScaleImpact = 1.5f;
+  public bool controlByPress = false; 
 
   [Header("Followers")]
   public int followersMin = 3;
