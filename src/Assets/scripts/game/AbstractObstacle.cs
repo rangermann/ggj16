69b54abfabeab8eb545a0691caaa1404c6028b1c
rangermann <rainer@rarebyte.com>
2016-01-30 10:01:08 +0100

    added test level to main scene

---
+++ src/Assets/scripts/game/AbstractObstacle.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public abstract class AbstractObstacle : MonoBehaviour {
+
+  public void OnTriggerEnter2D(Collider2D other) {
+    if (other.tag == "Player") {
+      
+    }
+  }
+
+
+}
