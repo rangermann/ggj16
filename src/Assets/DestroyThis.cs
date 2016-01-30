f7a403441cede27d86aefe6b446cce6df2a4fce3
Johanna Pirker <joeyprink@gmail.com>
2016-01-30 21:25:10 +0100

    explosion kill

---
+++ src/Assets/DestroyThis.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyThis : MonoBehaviour {
+
+	public float waitSecond = 1.7f;
+	public AnimationClip anim;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (KillOnAnimationEnd ());
+	}
+
+
+	private IEnumerator KillOnAnimationEnd() {
+
+		if (anim != null) {
+			waitSecond = anim.length;
+		}
+		yield return new WaitForSeconds (waitSecond);
+		Destroy (this.gameObject);
+	}
+
+	void Update () {
+		//StartCoroutine (KillOnAnimationEnd ());
+	}
+
+}
